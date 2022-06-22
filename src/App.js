fb80f0bc4254a55480669e20d04a75f49e91d889
Bruno Vitte <bruno_vitte@hotmail.com>
2022-06-22 12:56:34 -0500

    first draws

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+// ? CSS
+import 'materialize-css/dist/css/materialize.min.css'
+import './assets/css/App.scss'
+
+// ? Images
+import bussines from './assets/images/business-intelligence-1-1280x853.jpg'
+
+// * Components
+import Header from './navbar/component/header';
 
 function App() {
   return (
-    <div> Hello Oconexiones </div>
+    <>
+      <Header />
+      <div> Hello Oconexiones </div>
+      <img src={bussines} />
+    </>
   );
 }
 
---
+++ src/navbar/component/header.jsx
@@ -0,0 +1,21 @@
+// * React 
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper blue darken-3">
+          <a href="#" className="brand-logo left">Oconexciones</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">JavaScript</a></li>
+          </ul>
+        </div>
+      </nav>   
+    );
+  }
+}
+
+export default Header;
